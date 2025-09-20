ab5704db825fa51fb29d81e4a7622e28fa2b8704
therealkratika <therealkratikagupta@gmail.com>
2025-09-20 13:18:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174","https://campusvoice-backend.onrender.com"],
+  origin: ["http://localhost:5173","http://localhost:5174","https://campusvoice-admin.onrender.com"],
   credentials: true,
 }));
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
